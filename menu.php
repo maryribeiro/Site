4b8a20fc980677754a34ccca0eb68fd95b16b066
marileia_ribeiro1 <marileia_ribeiro1@SNRSL1ELBIET051>
2016-11-11 11:36:38 -0200

    editei  index

---
+++ menu.php
@@ -0,0 +1,6 @@
+<ul>
+    <li><a href="?pg=home">Home</a></li>
+    <li><a href="?pg=sobre">Sobre</a></li>
+    <li><a href="?pg=produtos">Produtos</a></li>
+    <li><a href="?pg=serviços">Serviços</a></li>
+</ul>
