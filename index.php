1daa65079006d3e4952d046ce12cfbd7573df08e
marileia_ribeiro1 <marileia_ribeiro1@SNRSL1ELBIET051>
2016-11-11 11:19:52 -0200

    editei  index

--- index.php
+++ index.php
@@ -1,17 +1,40 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title> Infofelb</title>
+        <link rel="stylesheet" href="css/estilo.css" type="text/css"/>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div id="container">
+            <div id="header">
+                <img src="img/logo.png">
+            </div>
+            <div id="menu">
+                <?php
+                include 'menu.php';
+                ?>
+            </div>
+            <div id="content">
+                <div id="content-main">
+                    <?php
+                    $pg = $_GET["pg"];
+                    if (!empty($pg)) {
+                        include $pg . '.php';
+                    } else {
+                        include 'home.php';
+                    }
+                    ?>
+                </div>
+                
+                    
+                </div>
+            </div>
+            <div id="footer">
+                <span>
+                    Todos os direitos reservados - &copy; - 2016.
+                </span>
+            </div>
+        </div>
     </body>
 </html>
