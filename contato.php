778b0dc361884b3a6ce10b5043510cd51d31e1a4
daiane_l_cunha <daiane_l_cunha@SNRSL1ELBIET049>
2016-11-16 10:58:30 -0200

    pagina de contato

---
+++ contato.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title> Contato </title>
+    </head>
+    <body>
+        <br>
+        <br>
+        <br>
+        <form method="post">
+            <center><fieldset>
+                Nome:
+               <br>
+                <input type="text" name="nome"/>
+                <br>
+                <br>
+                E-mail:
+                <br>
+                <input type="text" name="e-mail"/>
+                <br>
+                <br>
+                Coloque aqui sua duvida, sua sugestao ou reclamaçao:
+                <br>
+                <textarea></textarea>
+                <br>
+                <br>
+                <input type="submit" name="enviar"/>
+                <br>
+            </fieldset></center>
+        </form>
+        <?php
+        
+        
+        
+        ?>
+    </body>
+</html>
